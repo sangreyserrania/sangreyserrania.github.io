f37f9050bc1d3ac7d8ff436a64a68c3ec751b35d
lpalmes <lpalmes@gmail.com>
2019-02-06 22:01:06 -0300

    first commit

---
+++ src/pages/sangre.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Test = () => {
+  return (
+    <div class="container">
+      <div>YO SOY LA TELE</div>
+    </div>
+  );
+};
+
+export default Test;
